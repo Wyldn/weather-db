2271d68a7d07657cf1510d00f0033c6eaf58a4cf
RishiMannava <90156867+Wyldn@users.noreply.github.com>
2024-04-15 18:38:34 -0700

    js for displaying and pulling city

---
+++ script.js
@@ -0,0 +1,33 @@
+// Global variable declarations
+var cityList = [];
+var cityname;
+
+// local storage functions
+initCityList();
+initWeather();
+
+
+// DOM
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+    
+    for (i=0; i<cityList.length; i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    } 
+}
+
+// local sotrage
+function initCityList() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+    
+    if (storedCities !== null) {
+        cityList = storedCities;
+    }
+    
+    renderCities();
+    }
